53d4f0b77ef6e263f8c1dda7f681e2622ea7eb5a
0xarcano <prodasl@hotmail.com>
2025-04-13 20:40:33 -0500

    Completed pending testing

--- contracts/DAOFundNFT.sol
+++ contracts/DAOFundNFT.sol
@@ -6,13 +6,16 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./Base64.sol";
+import "./DAOFundDNA.sol";
 
-contract DAOFundNFT is ERC721, ERC721Enumerable {
+contract DAOFundNFT is ERC721, ERC721Enumerable, DAOFundDNA {
     using Counters for Counters.Counter;
 
+    address private immutable _deployer;
     Counters.Counter private _idCounter;
     uint public maxSupply;
     string private description;
+    mapping (uint256 => uint256) public tokenDNA;
 
     uint256 public priceUSD;
     uint256 public donationPercentage; // in percentage (1% = 1)
@@ -35,6 +38,7 @@ contract DAOFundNFT is ERC721, ERC721Enumerable {
         address _daiAddress
     ) ERC721(_name, _symbol) {
         require(_donationPercentage <= 100, "Donation percentage too high");
+        _deployer = msg.sender;
         description = _description;
         maxSupply = _maxSupply;
         priceUSD = _priceUSD;
@@ -45,20 +49,37 @@ contract DAOFundNFT is ERC721, ERC721Enumerable {
         daiToken = IERC20(_daiAddress);
     }
 
+    // This pseudo-random function is deterministic and should not be used for projects
+    // that require true randomness. YOU SHOULD IMPLEMENT YOUR OWN RANDOMNESS FOR PRODUCTION
+    // FOR PROJECTS THAT NEED TO RELY IN TRUE RANDOMNESS.
+    function deterministicPseudoRandomDNA(
+        uint256 _tokenId,
+        address _minter
+    ) public pure returns (uint256) {
+            require(_tokenId > 0, "Token ID must be greater than 0");
+            require(_minter != address(0), "Minter address cannot be zero");
+            uint256 combindedParams = _tokenId + uint256(uint160(_minter));
+            bytes memory encondedParams = abi.encodePacked(combindedParams);
+            bytes32 hasedParams = keccak256(encondedParams);
+
+            return uint256(hasedParams);
+    }
+
     function calculateTokenAmount() internal view returns (uint256) {
-        return priceUSD * 1e18;
+        return priceUSD * 1e6;
     }
 
     function calculateDonation(uint256 amount) internal view returns (uint256) {
         return (amount * donationPercentage) / 100;
     }
 
-    function mint(uint8 paymentMethod) external {
+    function mint(uint8 paymentMethod) public {
         uint256 current = _idCounter.current();
-        require(current < maxSupply, "No CypherHumas left :(");
+        require(current < maxSupply, string(abi.encodePacked("No ", name(), " left :(")));
         
         uint256 amount = calculateTokenAmount();
         uint256 donation = calculateDonation(amount);
+        uint256 totalAmount = amount;
         IERC20 token;
 
         if (paymentMethod == 0) { // USDC
@@ -71,11 +92,61 @@ contract DAOFundNFT is ERC721, ERC721Enumerable {
             revert("Invalid payment method");
         }
 
-        require(token.transferFrom(msg.sender, address(this), amount - donation), "Token transfer failed");
+        token.approve(address(this), totalAmount);
+        
+        require(token.transferFrom(msg.sender, _deployer , amount - donation), "Token transfer failed");
         require(token.transferFrom(msg.sender, daoTreasury, donation), "Donation transfer failed");
 
+        tokenDNA[current] = deterministicPseudoRandomDNA(current, msg.sender);
         _safeMint(msg.sender, current);
         _idCounter.increment();
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://avataaars.io/";
+    }
+
+function _paramsURI(uint256 _dna) internal view returns (string memory) {
+    string memory params;
+
+    {
+        params = string(
+            abi.encodePacked(
+                "accessoriesType=",
+                getAccessoriesType(_dna),
+                "&clotheColor=",
+                getClotheColor(_dna),
+                "&clotheType=",
+                getClotheType(_dna),
+                "&eyeType=",
+                getEyeType(_dna),
+                "&eyebrowType=",
+                getEyeBrowType(_dna),
+                "&facialHairColor=",
+                getFacialHairColor(_dna),
+                "&facialHairType=",
+                getFacialHairType(_dna),
+                "&hairColor=",
+                getHairColor(_dna),
+                "&hatColor=",
+                getHatColor(_dna),
+                "&graphicType=",
+                getGraphicType(_dna),
+                "&mouthType=",
+                getMouthType(_dna),
+                "&skinColor=",
+                getSkinColor(_dna)
+            )
+        );
+    }
+
+    return string(abi.encodePacked(params, "&topType=", getTopType(_dna)));
+}  
+
+    function imageByDNA(uint256 _dna) public view returns (string memory) {
+        string memory params = _paramsURI(_dna);
+        string memory baseURI = _baseURI();
+        return string(abi.encodePacked(baseURI, "?", params));
     }
 
     function tokenURI(uint256 _tokenId)
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,15 +2,56 @@ const deploy = async () => {
   const [deployer] = await ethers.getSigners();
   console.log("Deploying contracts with the account:", deployer.address);
 
-  const CypherHumas = await ethers.getContractFactory("CypherHumas");
-  const deployed = await CypherHumas.deploy();
+  const nftName = "EthEcuadorAvatars";
+  const nftSymbol = "EECA";
+  const nftDescription = "Eth Ecuador Avatars";
+  const nftMaxSupply = 100;
+  const nftMintPrice = 35;
+  const nftDaoTreasuryAddress = "0x88d2f5B400A7EF87bff74F66914019ec4FD1DD2d";
+  const nftDaoTreasuryDonationPercentage = 10;
+  const usdcContractAddress = "0x94a9D9AC8a22534E3FaCa9F4e7F2E2cf85d5E4C8";
+  const usdtContractAddress = "0xaA8E23Fb1079EA71e0a56F48a2aA51851D8433D0";
+  const daiContractAddress = "0xFF34B3d4Aee8ddCd6F9AFFFB6Fe49bD371b8a357";
 
-  console.log("CypherHumas deployed at:", deployed.address);
+  console.log("Deploying with parameters:");
+  console.log("NFT Name:", nftName);
+  console.log("NFT Symbol:", nftSymbol);
+  console.log("NFT Description:", nftDescription);
+  console.log("NFT Max Supply:", nftMaxSupply);
+  console.log("NFT Mint Price:", nftMintPrice);
+  console.log("NFT DAO Treasury Address:", nftDaoTreasuryAddress);
+  console.log("NFT DAO Treasury Donation Percentage:", nftDaoTreasuryDonationPercentage);
+  console.log("USDC Contract Address:", usdcContractAddress);
+  console.log("USDT Contract Address:", usdtContractAddress);
+  console.log("DAI Contract Address:", daiContractAddress);
+
+  try {
+    const DAOFundNFT = await ethers.getContractFactory("DAOFundNFT");
+    const deployed = await DAOFundNFT.deploy(
+      nftName,
+      nftSymbol,
+      nftDescription,
+      nftMaxSupply,
+      nftMintPrice,
+      nftDaoTreasuryAddress,
+      nftDaoTreasuryDonationPercentage,
+      usdcContractAddress,
+      usdtContractAddress,
+      daiContractAddress
+    );
+
+    console.log("EthEcuadorAvatars deployed at:", deployed.address);
+    await deployed.deployed(); // Wait for the contract to be fully deployed
+    console.log("Contract deployment confirmed.");
+
+  } catch (error) {
+    console.error("Deployment failed:", error);
+  }
 };
 
-deploy ()
+deploy()
   .then(() => process.exit(0))
   .catch((error) => {
-    console.error(error);
+    console.error("Unhandled error:", error);
     process.exit(1);
-  })
+  });
